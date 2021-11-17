2b7ed1e0a4d490f18c73e10a15cd0f0778d54dc4
Gosha <89145061+GoshaAB29@users.noreply.github.com>
2021-11-17 22:12:15 +0300

    bug fixes

--- main.cpp
+++ main.cpp
@@ -1,38 +1,9 @@
-#include "TXLib.h"
 #include "include/stack.h"
-
-#define STACK_TEST
-
-//TODO
-// log-file
-//
-//
+#include "include/stack_test.h"
 
 int main()
 {
-    Stack stk = {};
-    int error = 0;
-    #ifdef STACK_TEST
-    unsigned int capacity = 1;
-
-    error = Stac_Ctor (&stk, capacity);
-
-    for (int i = 0; i < 16; i++)
-        error = Stack_Push (&stk, i);
-
-    stack_t x = 1;
-
-    for (int i = 0; i < 22; i++) {
-    error = Stack_Pop (&stk, &x);
-    printf ("x = %d\n", x);
-    }
-    printf ("stk.capacity = %d\n", stk.capacity);
-    #endif
-
-    error = Stack_Dtor (&stk);
-    printf ("error number is %d\n", error);
-    error = Stack_Dtor (&stk);
-    printf ("error number is %d\n", error);
+    Test ();
 
     return 0;
 }
